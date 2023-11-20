3487d793f79a0f334e7c6c3ee48a14715d4142f8
William Pepple <williampepple1@gmail.com>
2023-11-20 11:58:10 +0100

    database config and user setup

--- models/user.go
+++ models/user.go
@@ -3,13 +3,40 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type RoleStatus string
+
+const (
+	StatusOwner   RoleStatus = "owner"
+	StatusManager RoleStatus = "manager"
+	StatusCashier RoleStatus = "cashier"
 )
 
 type User struct {
-	gorm.Model
-	Username string
-	Password string
-	Role     string // "cashier", "manager", "owner"
-	BranchID uint
+	ID        uuid.UUID  `gorm:"type:uuid;primary_key;" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	Username  string     `gorm:"unique" json:"username"`
+	Password  string     `json:"password"`
+	Role      RoleStatus `gorm:"type:varchar(20);not null;default:'to do'" json:"status"`
+	BranchID  uint
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (base *User) BeforeCreate(tx *gorm.DB) (err error) {
+	base.ID = uuid.New()
+	currentTime := time.Now()
+	base.CreatedAt = currentTime
+	base.UpdatedAt = currentTime
+	return nil
+}
+
+// GORM V2 uses callbacks like BeforeUpdate to handle the update timestamp
+func (user *User) BeforeUpdate(tx *gorm.DB) (err error) {
+	user.UpdatedAt = time.Now()
+	return
 }
